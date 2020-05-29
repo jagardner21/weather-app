6242f7a2dc5b3c11f3db718d4d50df056eae3d6b
jagardner21 <42302459+jagardner21@users.noreply.github.com>
2020-05-28 20:38:58 -0700

    store nearly complete, pending index.js

--- src/store/forecasts/actions.js
+++ src/store/forecasts/actions.js
@@ -3,3 +3,47 @@ import { GET_FIVE_DAY_BY_ZIP_PENDING, GET_FIVE_DAY_BY_ZIP_SUCCESS, GET_FIVE_DAY_
 
 const openWeatherAPIKey = "3738ec76690466dbf0f33c4d5262a246"
 const openWeatherBaseURL = "api.openweathermap.org/data/2.5/forecast"
+
+export const getFiveDayByZIP = zipCode => {
+    return dispatch => {
+        dispatch({
+            type: GET_FIVE_DAY_BY_ZIP_PENDING
+        })
+        axios.get(`${openWeatherBaseURL}?zip=${zipCode},us&appid=${openWeatherAPIKey}`)
+            .then(res => {
+                let fiveDayForecast = res.data
+                dispatch({
+                    type: GET_FIVE_DAY_BY_ZIP_SUCCESS,
+                    payload: fiveDayForecast
+                })
+            })
+            .catch(error => {
+                dispatch({
+                    type: GET_FIVE_DAY_BY_ZIP_FAILED,
+                    payload: error
+                })
+            })
+    }
+}
+
+export const getFiveDayByCity = (city, stateCode) => {
+    return dispatch => {
+        dispatch({
+            type: GET_FIVE_DAY_BY_CITY_PENDING
+        })
+        axios.get(`${openWeatherBaseURL}?q=${city},${stateCode}&appid=${openWeatherAPIKey}`)
+            .then(res => {
+                let fiveDayForecast = res.data
+                dispatch({
+                    type: GET_FIVE_DAY_BY_CITY_SUCCESS,
+                    payload: fiveDayForecast
+                })
+            })
+            .catch(error => {
+                dispatch({
+                    type: GET_FIVE_DAY_BY_CITY_FAILED,
+                    payload: error
+                })
+            })
+    }
+}
---
+++ src/store/forecasts/reducer.js
@@ -0,0 +1,39 @@
+import { GET_FIVE_DAY_BY_ZIP_PENDING, GET_FIVE_DAY_BY_ZIP_SUCCESS, GET_FIVE_DAY_BY_ZIP_FAILED, GET_FIVE_DAY_BY_CITY_PENDING, GET_FIVE_DAY_BY_CITY_SUCCESS, GET_FIVE_DAY_BY_CITY_FAILED } from './constants'
+
+let initialState = {
+    all: [],
+    fiveDayForecast: {},
+    err: {}
+}
+
+export default (state = initialState, action) => {
+    switch(action.type){
+        case GET_FIVE_DAY_BY_ZIP_PENDING:
+        case GET_FIVE_DAY_BY_CITY_PENDING:
+            return state;
+        
+        case GET_FIVE_DAY_BY_ZIP_FAILED:
+        case GET_FIVE_DAY_BY_CITY_FAILED:
+            let error = action.payload;
+            return {
+                ...state,
+                err: error
+            }
+
+        case GET_FIVE_DAY_BY_ZIP_SUCCESS:
+            let byZIPForecast = action.payload;
+            return {
+                ...state,
+                fiveDayForecast: byZIPForecast
+            }
+
+        case GET_FIVE_DAY_BY_CITY_SUCCESS: 
+            let byCityForecast = action.payload;
+            return {
+                ...state,
+                fiveDayForecast: byCityForecast
+            }
+        
+        default: return state;
+    }
+}
